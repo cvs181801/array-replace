88d1fda1458789d9380a420de3b4467cb8897f99
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-29 16:05:26 -0700

    comment out function

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let array1 = [1, 2, 1];
 const elemToReplace = 1;
 const substitutionElem = 3;
         
-//array.splice(0, 1, 3);
-//array.splice(2, 1, 3);
+array.splice(0, 1, 3);
+array.splice(2, 1, 3);
 
 
-function arrayReplace(array) {
-    array1 = [1, 2, 1];
-    array.splice(0, 1, 3);
-    array.splice(2, 1, 3);
-}
+// function arrayReplace(array) {
+//     array1 = [1, 2, 1];
+//     array.splice(0, 1, 3);
+//     array.splice(2, 1, 3);
+// }
 
 console.log(arrayReplace(array1));
